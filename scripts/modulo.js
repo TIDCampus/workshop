941aa99cbd46ac7332f41116b4621992a9b47990
Daniel Ruiz <daruga_89@outlook.com>
2013-09-26 20:28:07 +0200

    Create modulo con shell

--- scripts/modulo.js
+++ scripts/modulo.js
@@ -1,3 +1,9 @@
-function modulo(n,modulo){
-	return n%modulo;
-}
+mod=0;
+echo "Calcula el modulo"
+echo "============================="
+echo "Introducir el primer numero"
+read n1
+echo "Introducir el segundo numero"
+read n2
+let mod=n1%n2
+echo "El modulo es=" $mod;
