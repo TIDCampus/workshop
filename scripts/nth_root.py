ef48136e03c8659259975c7dd2db18e84cc78fb9
Alvaro Sanchez <alvaro.sperez@gmail.com>
2013-09-26 16:27:07 +0200

    nth_root script minor tweaks

--- scripts/nth_root.py
+++ scripts/nth_root.py
@@ -1,10 +1,7 @@
 print "Welcome to the nth root calculator"
 
 radicand = raw_input("Enter radicand: ")
-print "you entered ", radicand
-
 index = raw_input("Enter index: ")
-print "you entered ", index
 
 nroot = float(radicand)**(1.0/float(index))
 print "result: ", nroot
